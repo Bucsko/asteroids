8c79862453e36ae54286e536360a2a38a6b51a3c
a08uok <andras.bucsko@gmail.com>
2025-09-25 23:03:23 +0200

    add groups to handle mutiple object

--- asteroids/main.py
+++ asteroids/main.py
@@ -10,17 +10,29 @@ def main():
     pygame.get_init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
 
+    #creating managing goups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    #game objects
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
         #limit the frame rate to 60 fps
