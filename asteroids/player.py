1c5f86824133a08b43bf01d05c4c47dcebd2d622
a08uok <andras.bucsko@gmail.com>
2025-09-25 16:26:16 +0200

    add player turn

--- asteroids/player.py
+++ asteroids/player.py
@@ -1,11 +1,11 @@
 from circleshape import CircleShape
-import constants
+from constants import *
 import pygame
 
 class Player(CircleShape):
 
     def __init__(self, x, y):
-        super().__init__(x, y, constants.PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
     
     # in the player class
@@ -19,4 +19,17 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, (255, 255, 255), self.triangle(), 2)
-        
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+
+
